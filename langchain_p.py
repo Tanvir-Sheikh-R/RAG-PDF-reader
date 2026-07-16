605cecbf2a44b848ecc6d8ed9224b00c57306052
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-07-16 19:31:56 +0600

    demo

--- langchain_p.py
+++ langchain_p.py
@@ -2,32 +2,47 @@ from langchain_groq import ChatGroq
 from langchain.messages import HumanMessage, AIMessage, SystemMessage, ToolMessage
 from dotenv import load_dotenv
 from langchain.tools import tool
+from langchain_core.callbacks import StreamingStdOutCallbackHandler
+
+
+
+
+# import os
+# import openai
+
+# client = openai.OpenAI(
+#   base_url="https://api.groq.com/openai/v1",
+#   api_key=os.environ.get("GROQ_API_KEY")
+# )
+
+
 
 load_dotenv()
-message = [SystemMessage('You are a helpfull assistent who give answers in one line')]
+message = [SystemMessage('You are a helpfull assistent who give short answers')]
 
 model = ChatGroq(
-    model='llama-3.3-70b-versatile',
-    temperature=0.3
+    model='openai/gpt-oss-120b',
+    temperature=0.3,
+    streaming=True, 
+    callbacks=[StreamingStdOutCallbackHandler()]
 )
 
-@tool('web_search')
-def search(query: str) -> str:
-    """Search the web for information."""
-    return f"Results for: {query}"
-
+@tool
+def get_weather(city: str) -> str:
+    """Get the current weather for a given city."""
+    return f"It's sunny in {city}."
 
 @tool
 def add(x: int, y: int) -> int:
     """add two numbers"""
     return x + y
 
-tools = {'web_search': search, 'add': add}
-model_with_tools = model.bind_tools([search, add])
+tools = {'get_weather': get_weather, 'add': add}
+model_with_tools = model.bind_tools([get_weather, add])
 
 
 while True:
-    get_message : str = input('Enter your query... ')
+    get_message : str = input('Enter your query... \n')
     if get_message.lower() in ('exit', 'quit'):
         break
 
@@ -41,9 +56,15 @@ while True:
             result = tool_fn.invoke(call['args'])
             message.append(ToolMessage(content=str(result), tool_call_id=call['id']))
 
-        response = model_with_tools.invoke(message)
+        try:
+            response = model_with_tools.invoke(message)
+        except Exception as e:
+            print(e)
+            continue
         message.append(response)
 
+
+    
     print(response.content)
 
 print(message)
