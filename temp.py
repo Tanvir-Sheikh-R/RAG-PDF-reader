06ed0d83e1834fcf144ad3dc57f47b592c98f3c3
Tanvir-Sheikh-R <tanvirsheikh0177@gmail.com>
2026-07-17 18:52:53 +0600

    improved prompt

--- temp.py
+++ temp.py
@@ -7,13 +7,13 @@ load_dotenv()
 
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
-    temperature=0.7,
+    temperature=0.3,
     max_tokens=1024
 )
 
-st.title('Enter the paper you want to know about')
+st.title('Paper Summarizer',text_alignment='center')
 
-st.header('Paper Summarizer',text_alignment='center', width='stretch')
+st.header('Enter the paper you want to know about', width='stretch')
 
 paper_title = st.selectbox(label='Select paper title', options=['Attention is all you need', 'Prompt Engineering by Google x google', 'RAG for Knowledge-Intensive NLP Tasks'])
 
@@ -24,9 +24,16 @@ size = st.selectbox(label='Context Size', options=['Large', 'Mediam', 'Small'])
 
 tamplate = PromptTemplate(
         template="""
-        you are a smart Computer Science teacher. your task is to summarize this paper: {paper_title} in the tone of: {style} and in {size} length in context size,
-        explain math in there is any in the Paper mentioned.
-        give sample coding example if only asked
+            Please summarize the research paper titled "{paper_title}" with the following specifications:
+            Explanation Style: {style}
+            Explanation Length: {size}
+            1. Mathematical Details:
+                - Include relevant mathematical equations if present in the paper.
+                - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
+            2. Analogies:
+                - Use relatable analogies to simplify complex ideas.
+            If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
+            Ensure the summary is clear, accurate, and aligned with the provided style and length.
         """,
         input_variables=['paper_title', 'style', 'size'],
         validate_template=True
